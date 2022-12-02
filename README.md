16ab3af0f302de9fb41153a9e9354dfaf57d6849
rantao-usc <78400414+rantao-usc@users.noreply.github.com>
2022-12-02 15:13:49 -0800

    Revert to b66bcc4

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=9295136&assignment_repo_type=AssignmentRepo)
 # DSCI 554 Project
 
-Team name:
+Team name: Data Wizards
 
-Team members:
+Team members: Vedaanti Baliga, Sofian Ghazali, Ran Tao, Kyle Brooks
 
-- Tommy Trojan <tommy@usc.edu>
--
--
--
+- Vedaanti Baliga <vbaliga@usc.edu>
+- Sofian Ghazali <sghazali@usc.edu>
+- Ran Tao <taoran@usc.edu>
+- Kyle Brooks <kylebroo@usc.edu>
 
 ## Project summary (max 250 words)
 
@@ -23,27 +23,45 @@ Team members:
 
 ### Demo
 
-[Tommy Trojan](mailto:tommy@usc.edu):
+[Vedaanti Baliga](mailto:vbaliga@usc.edu):
 
-- did this  
-- did that
-- did this also
+- Developed the Vue app (dashboard, interactive sidebar, card components etc.)
+- Integrated the d3/html charts and ML analysis of all members into the Vue dashboard.
+- Created the proportional symbol map for electric vehicle counts (multiple maps).
+- Added a hover effect to Kyle's Pie Chart.
+
+[Kyle Brooks](mailto:kylebroo@usc.edu):
+
+- Created d3 choropleth chart with slider for transportation CO2 emissions statewide
+- Created d3 pie chart with dropdown for CO2 emissions by sector 
+
+[Sofian Ghazali](mailto:sghazali@usc.edu):
+
+-
+
+[Ran Tao](mailto:taoran@usc.edu):
+
+-
 
 ### Model performance analysis
 
-[Tommy Trojan](mailto:tommy@usc.edu):
+[Ran Tao](mailto:taoran@usc.edu):
 
 -
+
+[Sofian Ghazali](mailto:sghazali@usc.edu):
+
 -
--
+
 
 ### Paper
 
-[Tommy Trojan](mailto:tommy@usc.edu):
+[Kyle Brooks](mailto:kylebroo@usc.edu):
 
--
--
--
+- Introduction
+- Data
+- (Will) convert google doc of paper to latex format
+- (Will) manage references in bibliography
 
 ### Video
 
---
+++ vue3-app/src/views/Home.vue
@@ -0,0 +1,76 @@
+<template>
+	<main id="Home-page">
+		<h1>Effects of Electric Vehicles on Society</h1>
+		<p>Electric Vehicles (EVs) are becoming increasingly prominent in modern society, largely due to the environmental impact of gasoline powered vehicles. 
+			And it is estimated that by 2025 EVs will account for 30% of all vehicle sales, in stark contrast to 1% in 2016. In this dashboard, we have visualised data in various charts
+    to show the different statistics. Below is a summary of the charts that we have visualised, they can be accessed through the dashboard</p>
+      <div>
+        <b-container>
+          <b-row>
+            <b-col><b-card
+                    title="Multi line chart"
+                    img-src="card_component_elements/chart1.png"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                  >
+                  </b-card></b-col>
+                  <b-col> <b-card
+                    title="Zoommable circle packing"
+                    img-src="card_component_elements/chart2.png"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                  >
+                  </b-card></b-col>
+                  <b-col> <b-card
+                    title="Chloropleth map"
+                    img-src="card_component_elements/chart3.png"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                  >
+                  </b-card></b-col>
+                </b-row>
+                <b-row>
+            <b-col><b-card
+                    title="Pie Chart"
+                    img-src="card_component_elements/chart4.png"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                  >
+                  </b-card></b-col>
+                  <b-col> <b-card
+                    title="Proportional chart"
+                    img-src="card_component_elements/chart5.png"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                  >
+                  </b-card></b-col>
+                  <b-col> <b-card
+                    title="Predictive analysis"
+                    img-src="card_component_elements/chart6.png"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                  >
+                  </b-card></b-col>
+                </b-row>
+        </b-container>
+</div>
+  </main>
+</template>
